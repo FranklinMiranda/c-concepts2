bbc78b4b1b0d1486c2a9e438ed5af6d18a6766e1
FranklinMiranda <fdm61344@gmail.com>
2023-06-10 13:49:22 -0400

    Update to C Concepts Project

--- c-concepts2/c-concepts2.c
+++ c-concepts2/c-concepts2.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
+	FILE *fptr;
+	
+	// Creating a file 
+	fopen_s(&fptr, "filename.txt", "w");
+
+	// Closing an open file
+	fclose(&fptr);
+
+
+	printf("\nHello World!"); 
 
 
 	return 0;
